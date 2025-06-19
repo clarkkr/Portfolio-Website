8c71c2523cf52e9e12ef4703be62e1e8d3bed56b
clarkkr <kory.clark0517@gmail.com>
2025-06-19 15:45:51 -0400

    Set default theme to be dark.

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
@@ -33,7 +33,7 @@ export const ThemeToggle = () => {
       onClick={toggleTheme}
       className={cn(
         "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
+        "focus:outline-hidden"
       )}
     >
       {isDarkMode ? (
